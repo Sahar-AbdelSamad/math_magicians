f42d6316c11c2c9feb5c863cf068b01ca1a360aa
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-02-17 22:44:31 +0200

    Add unit tests for calculate and operate components

---
+++ src/__tests__/calculate.test.js
@@ -0,0 +1,54 @@
+import calculate from '../app/logic/calculate';
+
+describe('The result of calculate is correct', () => {
+  test('4 + 2 = 6', () => {
+    const object = {
+      total: '4',
+      next: '2',
+      operation: '+',
+    };
+
+    const result = calculate(object, '=');
+    expect(result.total).toEqual('6');
+  });
+  test('4 - 2 = 2', () => {
+    const object = {
+      total: '4',
+      next: '2',
+      operation: '-',
+    };
+
+    const result = calculate(object, '=');
+    expect(result.total).toEqual('2');
+  });
+  test('4 x 2 = 8', () => {
+    const object = {
+      total: '4',
+      next: '2',
+      operation: 'x',
+    };
+
+    const result = calculate(object, '=');
+    expect(result.total).toEqual('8');
+  });
+  test('4 % 2 = 0', () => {
+    const object = {
+      total: '4',
+      next: '2',
+      operation: '%', // result 6
+    };
+
+    const result = calculate(object, '=');
+    expect(result.total).toEqual('0');
+  });
+  test('4 ÷ 2 = 2', () => {
+    const object = {
+      total: '4',
+      next: '2',
+      operation: '÷', // result 6
+    };
+
+    const result = calculate(object, '=');
+    expect(result.total).toEqual('2');
+  });
+});
